0091f742a28409ace05207c40c1220c6b7b62ba1
Giorgos Kontopoulos <kongeo@gmail.com>
2020-11-20 15:06:19 +0200

    Remove PageTransitions component from individual routes

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,12 +1,9 @@
 <script>
 	import Counter from '$components/Counter.svelte';
-	import PageTransitions from '../components/PageTransitions.svelte';
 </script>
 
-<PageTransitions>
-  <main>
-    <h1>About Page</h1>
-    <p>This is the about page</p>
-    <p>More paragraphs of the about page</p>
-  </main>
-</PageTransitions>
+<main>
+  <h1>About Page</h1>
+  <p>This is the about page</p>
+  <p>More paragraphs of the about page</p>
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,10 @@
 <script>
 	import Counter from '$components/Counter.svelte';
-	import PageTransitions from '../components/PageTransitions.svelte';
 </script>
 
-<PageTransitions>
 <main>
 	<h1>Hello world!</h1>
 
 	<Counter/>
 	<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
 </main>
-</PageTransitions>
