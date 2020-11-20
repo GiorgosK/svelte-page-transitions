112e0d195ace8802913032730a5c22c489a930ca
Giorgos Kontopoulos <kongeo@gmail.com>
2020-11-20 15:07:47 +0200

    Make the PageTransitions component reactive and add it directly to the $layout component

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,8 +1,11 @@
 <script>
   import Nav from '../components/Nav';
+  import PageTransitions from '../components/PageTransitions';
   export let segment;	
 </script>
 
 <Nav {segment}/>
 
+<PageTransitions refresh={segment}>
   <slot/>
+</PageTransitions>
