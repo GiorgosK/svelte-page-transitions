db8e63ee061d76f1b66a39301c9b50bb4693429d
Giorgos Kontopoulos <kongeo@gmail.com>
2020-11-20 16:59:35 +0200

    Add github repo link to the demo

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -9,8 +9,13 @@
   .current {
     text-decoration: underline;
   }
+  .float-right {
+    float: right;
+    padding: 10px;
+  }
 </style>
 <div>
   <a href="/" class='{segment === undefined ? "current" : ""}'>Home</a>
   <a href="/about" class='{segment === "about" ? "current" : ""}'>About</a>
+  <a href="https://github.com/GiorgosK/svelte-page-transitions" class="float-right">GitHub Repo</a>
 </div>
