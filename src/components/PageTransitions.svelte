26e5b82637c5fce2e9bdfb0f7ae10543d2370ebd
Giorgos Kontopoulos <kongeo@gmail.com>
2020-11-20 13:53:42 +0200

    PageTransitions component used inside each route

---
+++ src/components/PageTransitions.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { fly } from 'svelte/transition';
+</script>
+
+  <div
+    in:fly="{{ y: -50, duration: 250, delay: 300 }}"
+    out:fly="{{ y: -50, duration: 250 }}" 
+    >
+    <slot/>
+  </div>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,12 @@
+<script>
+	import Counter from '$components/Counter.svelte';
+	import PageTransitions from '../components/PageTransitions.svelte';
+</script>
+
+<PageTransitions>
 <main>
 	<h1>About Page</h1>
   <p>This is the about page</p>
   <p>More paragraphs of the about page</p>
 </main>
+</PageTransitions>
